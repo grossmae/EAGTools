3018637bfca97e5b547097a0273a42296500e2cc
Evan Grossman <grossmae@gmail.com>
2016-10-19 20:16:16 -0700

    Initial Commit

---
+++ EAGTools/EAGTools.h
@@ -0,0 +1,19 @@
+//
+//  EAGTools.h
+//  EAGTools
+//
+//  Created by Evan Grossman on 10/19/16.
+//  Copyright © 2016 Evan Grossman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EAGTools.
+FOUNDATION_EXPORT double EAGToolsVersionNumber;
+
+//! Project version string for EAGTools.
+FOUNDATION_EXPORT const unsigned char EAGToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EAGTools/PublicHeader.h>
+
+
